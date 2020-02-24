2747bdca6c620b7a1fd54efe23d5123620f7989f
ningwei@xiaomi.com <ningwei@xiaomi.com>
2020-02-24 11:47:48 +0800

    t

--- spring-context/src/test/java/test/Test.java
+++ spring-context/src/test/java/test/Test.java
@@ -3,14 +3,12 @@ package test;
 import org.springframework.Persion;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.env.AbstractPropertyResolver;
 
 public class Test {
 	public static void main(String[] args) {
 //		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"classpath*:applicationContext.xml"/*,"classpath*:applicationContext.xml","classpath*:test.xml"*/}, true, null);
 		ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:applicationContext.xml");
 //		context.getParentBeanFact
-		((AbstractPropertyResolver)(context.getEnvironment())).setRequiredProperties("");
 		Persion persion = (Persion) context.getBean("persion");
 		System.out.println(persion.getP());
 //		for (String s : persion.getName()) {
