9ec16bfef69f50d91798c942ffd95e03f98d17e9
ningwei@xiaomi.com <ningwei@xiaomi.com>
2020-02-29 22:30:01 +0800

    t

--- spring-beans/src/main/java/org/springframework/beans/BeanWrapperImpl.java
+++ spring-beans/src/main/java/org/springframework/beans/BeanWrapperImpl.java
@@ -313,7 +313,7 @@ public class BeanWrapperImpl extends AbstractNestablePropertyAccessor implements
 		 */
 		@Override
 		public void setValue(final @Nullable Object value) throws Exception {
-			// 获得set<property/>的Method对象，例：public void setPersionB(PersionB)
+			// 获得set<property/>的Method对象
 			// pd就是 GenericTypeAwarePropertyDescriptor 实例
 			final Method writeMethod = (this.pd instanceof GenericTypeAwarePropertyDescriptor ?
 					((GenericTypeAwarePropertyDescriptor) this.pd).getWriteMethodForActualAccess() :
