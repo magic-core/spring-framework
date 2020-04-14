f78398357069999ebafbdba07c1a68592f561391
ningwei@xiaomi.com <ningwei@xiaomi.com>
2020-04-14 18:56:04 +0800

    t

--- spring-context/src/test/java/test/Test.java
+++ spring-context/src/test/java/test/Test.java
@@ -5,6 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
+
+	// Spring Demo
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:applicationContext.xml");
 		PersionA persion = (PersionA) context.getBean("persionA");
